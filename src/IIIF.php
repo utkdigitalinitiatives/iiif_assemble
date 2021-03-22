f584e0c2a977bbba13ed378c0eaaf40171d6bbfc
Mat Jordan <mat@utk.edu>
2021-03-22 16:23:16 -0400

    Add metadata walker.

--- src/IIIF.php
+++ src/IIIF.php
@@ -4,13 +4,14 @@ namespace Src;
 
 class IIIF {
 
-    private $xml;
+    private $mods;
     private $model;
     private $id;
 
-    public function __construct($xml, $model)
+    public function __construct($mods, $model)
     {
-        $this->xml = $xml;
+
+        $this->mods = $mods;
         $this->model = $model;
         $this->id = 'https//:' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 
@@ -18,23 +19,58 @@ class IIIF {
 
     public function buildManifest () {
 
-        $manifest = self::initManifest();
+//        print_r ($this->mods);
+
+        $manifest['@context'] = 'https://iiif.io/api/presentation/3/context.json';
+        $manifest['id'] = $this->id;
+        $manifest['type'] = 'Manifest';
+        $manifest['label'] = self::getLanguageArray($this->mods->titleInfo->title);
+        $manifest['summary'] = self::getLanguageArray($this->mods->abstract);
+        $manifest['metadata'] = self::buildMetadata();
 
         return json_encode($manifest);
 
     }
 
-    public function initManifest () {
+    public function buildMetadata () {
 
-        $document['@context'] = 'http://iiif.io/api/presentation/3/context.json';
-        $document['id'] = $this->id;
-        $document['type'] = 'Manifest';
+        $identifier = self::getLabelValuePair(
+            'Publication Identifier',
+            $this->mods->identifier
+        );
 
-        return $document;
+        $tableOfContents = self::getLabelValuePair(
+            'Table of Contents',
+            $this->mods->tableOfContents
+        );
+
+        $date = self::getLabelValuePair(
+            'Date',
+            $this->mods->originInfo->dateCreated
+        );
+
+        return (object) [
+            $identifier,
+            $tableOfContents,
+            $date
+        ];
 
     }
 
-    public static function buildLabel ($mods) {
+    public function getLabelValuePair ($label, $value) {
+
+        return (object) [
+            'label' => self::getLanguageArray($label),
+            'value' => self::getLanguageArray($value)
+        ];
+
+    }
+
+    public function getLanguageArray ($string, $language = 'en') {
+
+        return (object) [
+            $language => $string
+        ];
 
     }
 
