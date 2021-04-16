ffa3aa2349b67cde9199b61e8c38e663137c05d1
Mat Jordan <mat@utk.edu>
2021-04-16 15:11:48 -0400

    Add Jack Reed's IIIF-Leaflet cropper to tools suite.

---
+++ tools/cropping/app.js
@@ -0,0 +1,38 @@
+var baseUrl = 'https://digital.lib.utk.edu/iiif/2/collections~islandora~object~galston%3A700~datastream~OBJ';
+var map = L.map('map', {
+    center: [0, 0],
+    crs: L.CRS.Simple,
+    zoom: 0,
+});
+
+var iiifLayer = L.tileLayer.iiif(baseUrl + '/info.json', {
+    tileSize: 512
+}).addTo(map);
+
+var areaSelect = L.areaSelect({
+    width:100, height:100
+});
+
+areaSelect.addTo(map);
+
+$('#urlArea').html(baseUrl)
+
+areaSelect.on('change', function() {
+    var bounds = this.getBounds();
+    var zoom = map.getZoom();
+    var min = map.project(bounds.getSouthWest(), zoom);
+    var max = map.project(bounds.getNorthEast(), zoom);
+    var imageSize = iiifLayer._imageSizes[zoom];
+    var xRatio = iiifLayer.x / imageSize.x;
+    var yRatio = iiifLayer.y / imageSize.y;
+    var region = [
+        Math.floor(min.x * xRatio),
+        Math.floor(max.y * yRatio),
+        Math.floor((max.x - min.x) * xRatio),
+        Math.floor((min.y - max.y) * yRatio)
+    ];
+    var url = baseUrl + '/' + region.join(',') + '/full/0/default.jpg';
+    $('#urlArea').html(
+        '<a href="' + url + '" target=_blank>' + url + '</a>'
+    )
+});
