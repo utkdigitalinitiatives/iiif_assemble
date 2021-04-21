9edd0a1ef65644169cf8bba64caa65aaf56aeaea
Mat Jordan <mat@utk.edu>
2021-04-21 01:39:57 -0400

    Testing provider as viewer load issue.

--- src/IIIF.php
+++ src/IIIF.php
@@ -31,7 +31,7 @@ class IIIF {
         $manifest['summary'] = self::getLanguageArray($this->xpath->query('abstract'), 'value');
         $manifest['metadata'] = self::buildMetadata();
         $manifest['rights'] = self::buildRights();
-        $manifest['provider'] = self::buildProvider();
+//        $manifest['provider'] = self::buildProvider();
 //        $manifest['thumbnail'] = self::buildThumbnail(200, 200);
         $manifest['items'] = self::buildItems($id);
 
