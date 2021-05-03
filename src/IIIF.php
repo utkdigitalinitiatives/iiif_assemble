307f5b9ca9092fd329b2e54bc6323a73ab5fa64b
Mat Jordan <mat@utk.edu>
2021-05-03 12:47:47 -0400

    Fix xpath casing issue on label.

--- src/IIIF.php
+++ src/IIIF.php
@@ -33,7 +33,7 @@ class IIIF {
         $manifest['@context'] = ['https://iiif.io/api/presentation/3/context.json'];
         $manifest['id'] = $id;
         $manifest['type'] = 'Manifest';
-        $manifest['label'] = self::getLanguageArray($this->xpath->query('titleInfo[not(@type="Alternative")]'), 'value');
+        $manifest['label'] = self::getLanguageArray($this->xpath->query('titleInfo[not(@type="alternative")]'), 'value');
         $manifest['summary'] = self::getLanguageArray($this->xpath->query('abstract'), 'value');
         $manifest['metadata'] = self::buildMetadata();
         $manifest['rights'] = self::buildRights();
