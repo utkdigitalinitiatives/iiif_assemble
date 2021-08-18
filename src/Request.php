d59bb1b268930ef624f78d617414674d68d8a4db
Mark Baggett <mbagget1@utk.edu>
2021-08-18 12:39:55 -0400

    Initialize RISearch request.

--- src/Request.php
+++ src/Request.php
@@ -96,6 +96,19 @@ class Request {
 
     }
 
+    public static function getBibframeDuration($pid, $format = 'csv') {
+
+        $request = $_ENV['FEDORA_URL'] . '/risearch?type=tuples&lang=sparql&format=' . $format .'&query=';
+
+        $query = "PREFIX bibframe: <http://id.loc.gov/ontologies/bibframe/#>";
+        $query .= "SELECT \$duration FROM <#ri> WHERE {{ <info:fedora/" . $pid ."> bibframe:duration ?duration . }}";
+
+        $request .= self::escapeQuery($query);
+
+        return self::curlRequest($request);
+
+    }
+
     public static function getDatastream ($dsid, $pid, $format = 'XML') {
 
         $request = $_ENV['FEDORA_URL'] . '/objects/' . $pid;
