10828ccc607f802bc8fddf8f52adf822b44442a5
Mark Baggett <mbagget1@utk.edu>
2021-11-02 14:48:06 -0400

    Unquote duration builder.

--- src/IIIF.php
+++ src/IIIF.php
@@ -647,13 +647,12 @@ class IIIF {
     }
 
     private function getBibframeDuration($dsid) {
-//        $durations = Request::getBibframeDuration($this->pid, $dsid, 'csv');
-//        $duration = explode("\n", $durations['body'])[1];
-//        $split_duration = explode(":", $duration);
-//        $hours = intval($split_duration[0]) *  60 * 60;
-//        $minutes = intval($split_duration[1]) * 60;
-//        return $hours + $minutes + intval($split_duration[2]);
-        return 300;
+        $durations = Request::getBibframeDuration($this->pid, $dsid, 'csv');
+        $duration = explode("\n", $durations['body'])[1];
+        $split_duration = explode(":", $duration);
+        $hours = intval($split_duration[0]) *  60 * 60;
+        $minutes = intval($split_duration[1]) * 60;
+        return $hours + $minutes + intval($split_duration[2]);
     }
 
     private static function determineTypeByModel ($islandoraModel) {
