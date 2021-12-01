f8369b67b362638f9187b41cf4c860b9d22fa5b6
Mark Baggett <mbagget1@utk.edu>
2021-12-01 08:05:15 -0500

    Remove determine language method.

--- src/IIIF.php
+++ src/IIIF.php
@@ -107,19 +107,11 @@ class IIIF {
             'Título' => $this->xpath->query('titleInfo[@lang="spa"]/title'),
             'Publication Identifier' => $this->xpath->queryFilterByAttribute('identifier', false, 'type', ['issn','isbn']),
             'Browse' => $this->browse_sanitize($this->xpath->query('note[@displayLabel="Browse"]')),
-            'Language' => $this->determine_language($this->xpath->query('language/languageTerm'))
+            'Language' => $this->xpath->query('language/languageTerm')
         );
         $metadata_with_names = $this->add_names_to_metadata($metadata);
         return self::validateMetadata($metadata_with_names);
 
-    }
-
-    private function determine_language($value) {
-        $language = array('English');
-        if (count($value) > 0){
-            $language = $value;
-        }
-        return $language;
     }
 
     private function browse_sanitize($value) {
