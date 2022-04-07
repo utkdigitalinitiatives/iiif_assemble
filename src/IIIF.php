cf8bc1326dcb492bb98dd7b1587b471995822d8d
Mark Baggett <mbagget1@utk.edu>
2022-04-07 11:34:00 -0400

    Address DIGITAL-1378.

--- src/IIIF.php
+++ src/IIIF.php
@@ -528,10 +528,12 @@ class IIIF {
             $body['width'] = $response->width;
             $body['height'] = $response->height;;
             $body['format'] = "image/jpeg";
-            $body['service'] = (object) [
-                '@id' => $response->{'@id'},
-                '@type' => $response->{'@context'},
-                'profile' => $response->profile[0],
+            $body['service'] = [
+                (object) [
+                    '@id' => $response->{'@id'},
+                    '@type' => $response->{'@context'},
+                    'profile' => $response->profile[0],
+                    ]
             ];
         else :
             $body['id'] = $fallback;
