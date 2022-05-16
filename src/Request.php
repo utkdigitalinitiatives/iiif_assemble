6fcdff81c88b578ea7b203d5bffa1f351057fb98
Mark Baggett <mbagget1@utk.edu>
2022-05-16 14:38:22 -0400

    Make getMetadataObjects follow standard escaping.

--- src/Request.php
+++ src/Request.php
@@ -197,7 +197,7 @@ class Request {
 
     public static function getMetadataObjects ($field, $value, $format = 'csv') {
         $request = $_ENV['FEDORA_URL'] . '/risearch?type=tuples&lang=sparql&format=' . $format .'&query=';
-        $query = 'SELECT ?pid ?label FROM <#ri> WHERE {{ ?pid <' . $field . '> "'  . $value . '" ; <info:fedora/fedora-system:def/model#label> ?label . }}';
+        $query = "SELECT \$pid \$label FROM <#ri> WHERE {{ \$pid <" . $field . "> '"  . $value . "' ; <info:fedora/fedora-system:def/model#label> \$label . }}";
         $request .= self::escapeQuery($query);
 
         return self::curlRequest($request);
